Changed metric calculation, changed to smaller images, fixed nan bug.

fastmri/data/mri_data.py
@@ -575,12 +575,12 @@ class CESTDataset(VolumeDataset):
                  num_offsets: int = 8):
         super().__init__(root, challenge, transform, use_dataset_cache, sample_rate,
                          volume_sample_rate, dataset_cache_file, num_cols, cache_path)
-        self.cest_transform = lambda x, o: x
+        # self.cest_transform = lambda x, o: x
         self.num_offsets = num_offsets
 
     def apply_virtual_cest_contrast(self, kspace, target, offset: int):
         # self.cest_transform(volume, offset)
-        random_num = np.random.rand() + 1e-6
+        random_num = 10 * np.random.rand() + 1e4
         kspace = deepcopy(kspace) * random_num
         target = deepcopy(target) * random_num
         return kspace, target
@@ -613,7 +613,6 @@ class CESTDataset(VolumeDataset):
             fname, metadata = self.examples[i]
 
         samples = []
-        
 
         with h5py.File(fname, "r") as hf:
             kspace = np.asarray(hf["kspace"])
@@ -655,7 +654,8 @@ if __name__ == "__main__":
     mask = create_mask_for_mask_type("equispaced_fraction_3d", [0.08], [2])
     transform = VarNetDataTransformVolume4D(mask_func=mask, use_seed=False)
     # cest_ds = CESTDataset("/home/woody/iwi5/iwi5044h/fastMRI/multicoil_train", "multicoil", transform, use_dataset_cache=False, cache_path="/home/woody/iwi5/iwi5044h/Code/fastMRI/cache_test")
-    cest_ds = CESTDataset("/media/lukas/Hard/multicoil_train", "multicoil", transform, use_dataset_cache=False, cache_path="/media/lukas/Hard/cache_test")
+    cest_ds = CESTDataset(r"E:\Lukas\multicoil_train", "multicoil", transform, use_dataset_cache=False,
+                          cache_path=r"E:\Lukas\cache_test")
     
     for i in range(len(cest_ds)):
         item = cest_ds.__getitem__(i)
@@ -664,12 +664,12 @@ if __name__ == "__main__":
 
             mask = item.mask[:, offset].numpy().squeeze()
             vol = item.target[offset].numpy().squeeze()
-            plt.imshow(mask[..., 0])
-            plt.title(f"Sample {i}, offset {offset}")
-            plt.show()
-            vol = (vol - vol.min()) / (vol.max() - vol.min())
-            vol = np.moveaxis(vol, 0, -1)
-            scroll_slices(vol, title=f"Sample {i} Offset {offset}")
+            # plt.imshow(mask[..., 0])
+            # plt.title(f"Sample {i}, offset {offset}")
+            # plt.show()
+            # vol = (vol - vol.min()) / (vol.max() - vol.min())
+            # vol = np.moveaxis(vol, 0, -1)
+            # scroll_slices(vol, title=f"Sample {i} Offset {offset}")
             print(f"Mean target: {np.mean(vol):.3g} Mean kspace {np.mean(item.masked_kspace[offset].numpy().squeeze()):.3g}")
 
     # varnet = VarNet4D(8, 2, 2, 2, 2).cuda()

fastmri/evaluate.py
@@ -13,7 +13,7 @@ from typing import Optional
 import h5py
 import numpy as np
 from runstats import Statistics
-from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity, normalized_root_mse
 from fastmri.losses import ssim3D
 
 from fastmri.data import transforms
@@ -22,15 +22,15 @@ from fastmri.data import transforms
 def mse(gt: np.ndarray, pred: np.ndarray) -> np.ndarray:
     """Compute Mean Squared Error (MSE)"""
     gt = (gt - gt.min()) / (gt.max() - gt.min())
-    pred = (pred - pred.min()) / (pred.max() - pred.min())
+    pred = (pred - pred.min()) / (pred.max() - pred.min() + 1e-6)
     return np.mean((gt - pred) ** 2)
 
 
 def nmse(gt: np.ndarray, pred: np.ndarray) -> np.ndarray:
     """Compute Normalized Mean Squared Error (NMSE)"""
-    gt = (gt - gt.min()) / (gt.max() - gt.min())
-    pred = (pred - pred.min()) / (pred.max() - pred.min())
-    return np.linalg.norm(gt - pred) ** 2 / np.linalg.norm(gt) ** 2
+    # gt = (gt - gt.min()) / (gt.max() - gt.min())
+    # pred = (pred - pred.min()) / (pred.max() - pred.min())
+    return normalized_root_mse(gt, pred)  # np.linalg.norm(gt - pred) ** 2 / np.linalg.norm(gt) ** 2
 
 
 def psnr(
@@ -42,7 +42,7 @@ def psnr(
     maxval = None
     if maxval is None:
         maxval = gt.max()
-    return peak_signal_noise_ratio(gt, pred, data_range=maxval)
+    return np.abs(peak_signal_noise_ratio(gt, pred, data_range=maxval))
 
 
 def ssim(

fastmri/models/varnet_4d.py
@@ -73,7 +73,8 @@ class NormUnet(nn.Module):
 
         x = x.view(b, c, o, d, h, w)
 
-        return (x - mean) / std, mean, std
+        res = (x - mean) / (std + 1e-5), mean, std + 1e-5
+        return res
 
     def unnorm(
         self, x: torch.Tensor, mean: torch.Tensor, std: torch.Tensor
@@ -117,11 +118,13 @@ class NormUnet(nn.Module):
         x = self.complex_to_chan_dim(x)
         x, mean, std = self.norm(x)
         x, pad_sizes = self.pad(x)
-        x = self.unet(x)
+        x_pre = x
+        x = self.unet(x_pre)
         # get shapes back and unnormalize
         x = self.unpad(x, *pad_sizes)
         x = self.unnorm(x, mean, std)
         x = self.chan_complex_to_last_dim(x)
+
         return x
 
 
@@ -212,10 +215,12 @@ class SensitivityModel(nn.Module):
             masked_kspace = transforms.batched_mask_center(
                 masked_kspace, pad, pad + num_low_freqs
             )
-
+        if torch.isnan(masked_kspace).any():
+            print()
         # convert to image space
         images, batches = self.chans_to_batch_dim(fastmri.ifft3c_new_offsets(masked_kspace))
-
+        if torch.isnan(images).any():
+            print()
         # estimate sensitivities
         ret = self.divide_root_sum_of_squares(
             self.batch_chans_to_chan_dim(self.norm_unet(images), batches)
@@ -254,12 +259,12 @@ class VarNet4D(nn.Module):
                 calculation.
         """
         super().__init__()
-        print("Sensitivity model disabled.")
-        # self.sens_net = SensitivityModel(
-        #     chans=sens_chans,
-        #     num_pools=sens_pools,
-        #     mask_center=mask_center,
-        # )
+        # print("Sensitivity model disabled.")
+        self.sens_net = SensitivityModel(
+            chans=sens_chans,
+            num_pools=sens_pools,
+            mask_center=mask_center,
+        )
         self.cascades = nn.ModuleList(
             [VarNetBlock(NormUnet(chans, pools)) for _ in range(num_cascades)]
         )
@@ -270,8 +275,8 @@ class VarNet4D(nn.Module):
         mask: torch.Tensor,
         num_low_frequencies: Optional[int] = None,
     ) -> torch.Tensor:
-        # sens_maps = self.sens_net(masked_kspace, mask, num_low_frequencies)
-        sens_maps = torch.ones_like(masked_kspace)
+        sens_maps = self.sens_net(masked_kspace, mask, num_low_frequencies)
+        # sens_maps = torch.ones_like(masked_kspace)
         kspace_pred = masked_kspace.clone()
 
         for cascade in self.cascades:

fastmri/pl_modules/mri_module.py
@@ -15,6 +15,7 @@ import pytorch_lightning as pl
 import torch
 from fastmri import evaluate
 from torchmetrics.metric import Metric
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity, normalized_root_mse
 
 
 class DistributedMetricSum(Metric):
@@ -60,12 +61,12 @@ class MriModule(pl.LightningModule):
         self.num_log_images = num_log_images
         self.val_log_indices = None
 
-        self.NMSE = DistributedMetricSum()
-        self.SSIM = DistributedMetricSum()
-        self.PSNR = DistributedMetricSum()
-        self.ValLoss = DistributedMetricSum()
-        self.TotExamples = DistributedMetricSum()
-        self.TotSliceExamples = DistributedMetricSum()
+        # self.NMSE = DistributedMetricSum()
+        # self.SSIM = DistributedMetricSum()
+        # self.PSNR = DistributedMetricSum()
+        # self.ValLoss = DistributedMetricSum()
+        # self.TotExamples = DistributedMetricSum()
+        # self.TotSliceExamples = DistributedMetricSum()
 
     def validation_step_end(self, val_logs):
         # check inputs
@@ -82,6 +83,14 @@ class MriModule(pl.LightningModule):
                 raise RuntimeError(
                     f"Expected key {k} in dict returned by validation_step."
                 )
+
+        target = val_logs["target"].cpu().numpy()
+        target = (target - target.min()) / (target.max() - target.min() + 1e-6)
+        prediction = val_logs["output"].cpu().numpy()
+        prediction = (prediction - prediction.min()) / (prediction.max() - prediction.min() + 1e-6)
+        self.log("val_metrics/psnr", peak_signal_noise_ratio(target, prediction))
+        self.log("val_metrics/nrmse", normalized_root_mse(target, prediction))
+
         if val_logs["output"].ndim == 2:
             val_logs["output"] = val_logs["output"].unsqueeze(0)
         # Removed for volume training
@@ -108,9 +117,9 @@ class MriModule(pl.LightningModule):
                 target = val_logs["target"][i].unsqueeze(0)
                 output = val_logs["output"][i].unsqueeze(0)
                 error = torch.abs(target - output)
-                output = output / output.max()
-                target = target / target.max()
-                error = error / error.max()
+                output = (output - output.min()) / (output.max() - output.min() + 1e-6)
+                target = (target - target.min()) / (target.max() - target.min())
+                error = (error - error.min()) / (error.max() - error.min())
                 center_slice = target.shape[1] // 2
                 self.log_image(f"{key}/target", target[:, 2, 2, ...])
                 self.log_image(f"{key}/reconstruction", output[:, 2, 2, ...])
@@ -125,114 +134,114 @@ class MriModule(pl.LightningModule):
                         mask = mask.squeeze()
                         mask = mask.tile((mask.shape[0], 1)).unsqueeze(0)
                     self.log_image(f"{key}/mask", mask)
-
-        # compute evaluation metrics
-        mse_vals = defaultdict(dict)
-        target_norms = defaultdict(dict)
-        ssim_vals = defaultdict(dict)
-        max_vals = dict()
-        for i, fname in enumerate(val_logs["fname"]):
-            slice_num = int(val_logs["slice_num"][i].cpu())
-            maxval = val_logs["max_value"][i].cpu().numpy()
-            output = val_logs["output"][i].cpu().numpy()
-            target = val_logs["target"][i].cpu().numpy()
-
-            mse_vals[fname][slice_num] = torch.tensor(
-                evaluate.mse(target, output)
-            ).view(1)
-            target_norms[fname][slice_num] = torch.tensor(
-                evaluate.mse(target, np.zeros_like(target))
-            ).view(1)
-            # ssim_vals[fname][slice_num] = torch.tensor(
-            #     evaluate.ssim(target[None, ...], output[None, ...], maxval=maxval)
-            # ).view(1)
-            # ssim_vals[fname][slice_num] = torch.tensor(
-            #     evaluate.ssim3D(target[None, ...], output[None, ...])
-            # ).view(1)
-
-            max_vals[fname] = maxval
+        # # compute evaluation metrics
+        # mse_vals = defaultdict(dict)
+        # target_norms = defaultdict(dict)
+        # ssim_vals = defaultdict(dict)
+        # max_vals = dict()
+        # for i, fname in enumerate(val_logs["fname"]):
+        #     slice_num = int(val_logs["slice_num"][i].cpu())
+        #     maxval = val_logs["max_value"][i].cpu().numpy()
+        #     output = val_logs["output"][i].cpu().numpy()
+        #     target = val_logs["target"][i].cpu().numpy()
+        #
+        #     mse_vals[fname][slice_num] = torch.tensor(
+        #         evaluate.mse(target, output)
+        #     ).view(1)
+        #     target_norms[fname][slice_num] = torch.tensor(
+        #         evaluate.mse(target, np.zeros_like(target))
+        #     ).view(1)
+        #     # ssim_vals[fname][slice_num] = torch.tensor(
+        #     #     evaluate.ssim(target[None, ...], output[None, ...], maxval=maxval)
+        #     # ).view(1)
+        #     # ssim_vals[fname][slice_num] = torch.tensor(
+        #     #     evaluate.ssim3D(target[None, ...], output[None, ...])
+        #     # ).view(1)
+        #
+        #     max_vals[fname] = maxval
 
             return {
             "val_loss": val_logs["val_loss"],
-            "mse_vals": dict(mse_vals),
-            "target_norms": dict(target_norms),
-            # "ssim_vals": dict(ssim_vals),
-            "max_vals": max_vals,
+            # "mse_vals": dict(mse_vals),
+            # "target_norms": dict(target_norms),
+            # # "ssim_vals": dict(ssim_vals),
+            # "max_vals": max_vals,
         }
 
     def log_image(self, name, image):
         self.logger.experiment.add_image(name, image, global_step=self.global_step)
 
     def validation_epoch_end(self, val_logs):
+        pass
         # aggregate losses
-        losses = []
-        mse_vals = defaultdict(dict)
-        target_norms = defaultdict(dict)
-        ssim_vals = defaultdict(dict)
-        max_vals = dict()
-
-        # use dict updates to handle duplicate slices
-        for val_log in val_logs:
-            losses.append(val_log["val_loss"].view(-1))
-
-            for k in val_log["mse_vals"].keys():
-                mse_vals[k].update(val_log["mse_vals"][k])
-            for k in val_log["target_norms"].keys():
-                target_norms[k].update(val_log["target_norms"][k])
-            # for k in val_log["ssim_vals"].keys():
-            #     ssim_vals[k].update(val_log["ssim_vals"][k])
-            for k in val_log["max_vals"]:
-                max_vals[k] = val_log["max_vals"][k]
-
-        # check to make sure we have all files in all metrics
-        assert (
-            mse_vals.keys()
-            == target_norms.keys()
-            # == ssim_vals.keys()
-            == max_vals.keys()
-        )
-
-        # apply means across image volumes
-        metrics = {"nmse": 0,
-                #    "ssim": 0,
-                   "psnr": 0}
-        local_examples = 0
-        for fname in mse_vals.keys():
-            local_examples = local_examples + 1
-            mse_val = torch.mean(
-                torch.cat([v.view(-1) for _, v in mse_vals[fname].items()])
-            )
-            target_norm = torch.mean(
-                torch.cat([v.view(-1) for _, v in target_norms[fname].items()])
-            )
-            metrics["nmse"] = metrics["nmse"] + mse_val / target_norm
-            metrics["psnr"] = (
-                metrics["psnr"]
-                + 20
-                * torch.log10(
-                    torch.tensor(
-                        max_vals[fname], dtype=mse_val.dtype, device=mse_val.device
-                    )
-                )
-                - 10 * torch.log10(mse_val)
-            )
-            # metrics["ssim"] = metrics["ssim"] + torch.mean(
-            #     torch.cat([v.view(-1) for _, v in ssim_vals[fname].items()])
-            # )
-
-        # reduce across ddp via sum
-        metrics["nmse"] = self.NMSE(metrics["nmse"])
-        # metrics["ssim"] = self.SSIM(metrics["ssim"])
-        metrics["psnr"] = self.PSNR(metrics["psnr"])
-        tot_examples = self.TotExamples(torch.tensor(local_examples))
-        val_loss = self.ValLoss(torch.sum(torch.cat(losses)))
-        tot_slice_examples = self.TotSliceExamples(
-            torch.tensor(len(losses), dtype=torch.float)
-        )
-
-        self.log("validation_loss", val_loss / tot_slice_examples, prog_bar=True)
-        for metric, value in metrics.items():
-            self.log(f"val_metrics/{metric}", value / tot_examples)
+        # losses = []
+        # mse_vals = defaultdict(dict)
+        # target_norms = defaultdict(dict)
+        # ssim_vals = defaultdict(dict)
+        # max_vals = dict()
+        #
+        # # use dict updates to handle duplicate slices
+        # for val_log in val_logs:
+        #     losses.append(val_log["val_loss"].view(-1))
+        #
+        #     for k in val_log["mse_vals"].keys():
+        #         mse_vals[k].update(val_log["mse_vals"][k])
+        #     for k in val_log["target_norms"].keys():
+        #         target_norms[k].update(val_log["target_norms"][k])
+        #     # for k in val_log["ssim_vals"].keys():
+        #     #     ssim_vals[k].update(val_log["ssim_vals"][k])
+        #     for k in val_log["max_vals"]:
+        #         max_vals[k] = val_log["max_vals"][k]
+        #
+        # # check to make sure we have all files in all metrics
+        # assert (
+        #     mse_vals.keys()
+        #     == target_norms.keys()
+        #     # == ssim_vals.keys()
+        #     == max_vals.keys()
+        # )
+        #
+        # # apply means across image volumes
+        # metrics = {"nmse": 0,
+        #         #    "ssim": 0,
+        #            "psnr": 0}
+        # local_examples = 0
+        # for fname in mse_vals.keys():
+        #     local_examples = local_examples + 1
+        #     mse_val = torch.mean(
+        #         torch.cat([v.view(-1) for _, v in mse_vals[fname].items()])
+        #     )
+        #     target_norm = torch.mean(
+        #         torch.cat([v.view(-1) for _, v in target_norms[fname].items()])
+        #     )
+        #     metrics["nmse"] = metrics["nmse"] + mse_val / target_norm
+        #     metrics["psnr"] = (
+        #         metrics["psnr"]
+        #         + 20
+        #         * torch.log10(
+        #             torch.tensor(
+        #                 max_vals[fname], dtype=mse_val.dtype, device=mse_val.device
+        #             )
+        #         )
+        #         - 10 * torch.log10(mse_val)
+        #     )
+        #     # metrics["ssim"] = metrics["ssim"] + torch.mean(
+        #     #     torch.cat([v.view(-1) for _, v in ssim_vals[fname].items()])
+        #     # )
+        #
+        # # reduce across ddp via sum
+        # metrics["nmse"] = self.NMSE(metrics["nmse"])
+        # # metrics["ssim"] = self.SSIM(metrics["ssim"])
+        # metrics["psnr"] = self.PSNR(metrics["psnr"])
+        # tot_examples = self.TotExamples(torch.tensor(local_examples))
+        # val_loss = self.ValLoss(torch.sum(torch.cat(losses)))
+        # tot_slice_examples = self.TotSliceExamples(
+        #     torch.tensor(len(losses), dtype=torch.float)
+        # )
+        #
+        # self.log("validation_loss", val_loss / tot_slice_examples, prog_bar=True)
+        # for metric, value in metrics.items():
+        #     self.log(f"val_metrics/{metric}", value / tot_examples)
 
     def test_epoch_end(self, test_logs):
         outputs = defaultdict(dict)

fastmri/pl_modules/varnet_module.py
@@ -16,12 +16,6 @@ from fastmri.models import VarNet, VarNet3D, VarNet4D
 from .mri_module import MriModule
 
 
-def mse_mod(x, y, data_range):
-    x = (x - x.min()) / (x.max() - x.min())
-    y = (y - y.min()) / (y.max() - y.min())
-    return F.mse_loss(x, y)
-
-
 class VarNetModule(MriModule):
     """
     VarNet training module.
@@ -108,8 +102,7 @@ class VarNetModule(MriModule):
 
         if volume_training:
             print("Using MSELoss")
-            self.loss = mse_mod
-            # self.loss = fastmri.SSIM3DLoss()
+            self.loss = torch.nn.MSELoss()
         else:
             self.loss = fastmri.SSIMLoss()
 
@@ -121,9 +114,8 @@ class VarNetModule(MriModule):
 
         target, output = transforms.center_crop_to_smallest(batch.target, output)
         loss = self.loss(
-            output.unsqueeze(1), target.unsqueeze(1), data_range=batch.max_value
+            output.unsqueeze(1), target.unsqueeze(1),  # data_range=batch.max_value
         )
-
         self.log("train_loss", loss)
 
         return loss
@@ -133,7 +125,10 @@ class VarNetModule(MriModule):
             batch.masked_kspace, batch.mask, batch.num_low_frequencies
         )
         target, output = transforms.center_crop_to_smallest(batch.target, output)
-
+        loss = self.loss(
+            output.unsqueeze(1), target.unsqueeze(1),  # data_range=batch.max_value
+        )
+        self.log("validation_loss", loss)
         return {
             "batch_idx": batch_idx,
             "fname": batch.fname,
@@ -142,9 +137,7 @@ class VarNetModule(MriModule):
             "output": output,
             "mask": batch.mask,
             "target": target,
-            "val_loss": self.loss(
-                output.unsqueeze(1), target.unsqueeze(1), data_range=batch.max_value
-            ),
+            "val_loss": loss
         }
 
     def test_step(self, batch, batch_idx):