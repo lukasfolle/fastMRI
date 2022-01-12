2b5e6aaf8a791a74fb95630c03bce3ab8f6e66d2
Folle, Lukas <lukas.folle@extern.uk-erlangen.de>
2022-01-12 17:46:43 +0100

    Slight changes to cs reco.

--- fastmri_examples/cs/adopted_3d_reco.m
+++ fastmri_examples/cs/adopted_3d_reco.m
@@ -2,8 +2,8 @@
 setenv('TOOLBOX_PATH', '');
 
 %% Load data
-simulation = 1;
-if ~simulation 
+simulation = 0;
+if ~simulation
     kspace = h5read('data/file1000000.h5', '/kspace');
     kspace = kspace.r + 1j * kspace.i;
 
@@ -24,19 +24,23 @@ for k=1:size(kspace, 4)
     knee_imgs(:, :, :, k) = ifftn(knee_imgs(:, :, :, k));
     knee_imgs(:, :, :, k) = ifftshift(knee_imgs(:, :, :, k));
 end
-knee_imgs = (knee_imgs - min(knee_imgs(:))) / (max(knee_imgs(:)) - min(knee_imgs(:)));
-knee_rss = squeeze(abs(sum(knee_imgs, 4) .^ 2));
-imagesc(squeeze(knee_rss(:, :, slice_sel)))
+% knee_imgs = (knee_imgs - min(knee_imgs(:))) / (max(knee_imgs(:)) - min(knee_imgs(:)));
+knee_rss = bart('rss 8', knee_imgs);
+knee_rss = (knee_rss - min(knee_rss(:))) / (max(knee_rss(:)) - min(knee_rss(:)));
+figure;
+imshow(squeeze(knee_rss(:, :, slice_sel)), [])
 
 %% Undersampling
-accel = 4.0;
-num_samples = 4800;
+accel = 6.0;
+num_samples = 5000;
 while true
     us_mask = zeros(size(knee_rss, 3), size(knee_rss, 2));
     mu = [round(size(knee_rss, 2) / 2) round(size(knee_rss, 3) / 2)];
     Sigma = [size(knee_rss, 2) * 10 0;
              0 size(knee_rss, 3) * 10];
     X = int32(mvnrnd(mu, Sigma, num_samples));
+    % Dense central mask sampling
+    us_mask(round(size(us_mask, 1)*2/8):round(size(us_mask, 1)*6/8),round(size(us_mask, 2)*9/20):round(size(us_mask, 2)*11/20)) = 1;
     for xi=1:length(X)
         idx = X(xi,:);
         if idx(1) < 1
@@ -66,10 +70,6 @@ imshow(squeeze(us_mask(:, :)'))
 us_mask = reshape(us_mask, 1, size(us_mask, 1), size(us_mask, 2));
 us_mask = repmat(us_mask, size(kspace, 1), 1, 1, size(kspace, 4));
 frac = 8;
-acs = kspace(:, round(size(kspace, 2) * (frac/2-1)/frac):round(size(kspace, 2) * (frac/2+1)/frac), round(size(kspace, 3) * (frac/2-1)/frac):round(size(kspace, 3) * (frac/2+1)/frac), :);
-pad = zeros(size(kspace));
-pad(:, round(size(kspace, 2) * (frac/2-1)/frac):round(size(kspace, 2) * (frac/2+1)/frac), round(size(kspace, 3) * (frac/2-1)/frac):round(size(kspace, 3) * (frac/2+1)/frac), :) = acs;
-acs = pad;
 kspace = kspace .* us_mask;
 
 %% Reconstruct undersampled kspace
@@ -80,21 +80,27 @@ for k=1:size(kspace, 4)
     knee_imgs(:, :, :, k) = ifftshift(knee_imgs(:, :, :, k));
 end
 
-knee_rss_us = squeeze(abs(sum(knee_imgs, 4) .^ 2));
+knee_rss_us = bart('rss 8', knee_imgs);
 knee_imgs = (knee_imgs - min(knee_imgs(:))) / (max(knee_imgs(:)) - min(knee_imgs(:)));
 
 figure;
 subplot(2,1,1);
-imagesc(squeeze(knee_rss(:, :, slice_sel)))
+imshow(squeeze(knee_rss(:, :, slice_sel)), [])
 title('No US')
 subplot(2,1,2);
-imagesc(squeeze(knee_rss_us(:, :, slice_sel)))
+imshow(squeeze(knee_rss_us(:, :, slice_sel)), [])
 title('With US')
 
 %% ESPIRiT calibration
-sens_maps = bart('ecalib -m 1', acs);
-% sens_maps = bart('ecalib -m1 -k 4', kspace); does not work, too few info?
+sens_maps = bart('ecalib -c0. -m1', kspace);
 
+figure;
+subplot(2,1,1)
+imshow3(abs(squeeze(sens_maps(:, :, 18, :))), [],[3,5]);
+title("Espirit Maps")
+subplot(2,1,2)
+imshow3(abs(squeeze(knee_imgs(:, :, 18, :))), [],[3,5]);
+title("Fully sampled images")
 %%
 %     0 readout
 %     1 phase-encoding dimension 1
@@ -102,20 +108,31 @@ sens_maps = bart('ecalib -m 1', acs);
 %     3 receive channels
 %     4 ESPIRiT maps
 
-kspace_l1 = bart('pics -l1', kspace, sens_maps);
-
-%%% ACS free CS method????????????????????????????????????
-
-
+kspace_l1 = bart('pics -l1 -r 0.01', kspace, sens_maps);
+kspace_l1 = abs(kspace_l1);
 
 %% Results
 figure;
-subplot(1,3,1);
-imagesc(squeeze(knee_rss(:, :, slice_sel)))
-title('No US')
-subplot(1,3,2);
-imagesc(squeeze(knee_rss_us(:, :, slice_sel)))
-title('With US')
-subplot(1,3,3)
-imagesc(squeeze(abs(kspace_l1(:, :, slice_sel))))
-title('With CS Reco')
+rss = squeeze(knee_rss(:, :, slice_sel));
+rss = (rss - min(rss(:))) / (max(rss(:)) - min(rss(:)));
+rss_us = squeeze(knee_rss_us(:, :, slice_sel));
+rss_us = (rss_us - min(rss_us(:))) / (max(rss_us(:)) - min(rss_us(:)));
+cs = squeeze(abs(kspace_l1(:, :, slice_sel)));
+cs = (cs - min(cs(:))) / (max(cs(:)) - min(cs(:)));
+
+imshow([rss; rss_us; cs], [])
+title('Original | US Reco | CS Reco')
+
+%% Performance calculation
+knee_rss = (knee_rss - min(knee_rss(:))) / (max(knee_rss(:)) - min(knee_rss(:)));
+knee_rss_us = (knee_rss_us - min(knee_rss_us(:))) / (max(knee_rss_us(:)) - min(knee_rss_us(:)));
+kspace_l1 = (kspace_l1 - min(kspace_l1(:))) / (max(kspace_l1(:)) - min(kspace_l1(:)));
+disp("SSIM")
+disp(ssim(knee_rss, knee_rss_us))
+disp(ssim(knee_rss, kspace_l1))
+disp("PSNR")
+disp(psnr(knee_rss,knee_rss_us))
+disp(psnr(knee_rss,kspace_l1))
+disp("NRMSE")
+disp(sqrt(mean((knee_rss(:) - knee_rss_us(:)).^2)))
+disp(sqrt(mean((knee_rss(:) - kspace_l1(:)).^2)))
